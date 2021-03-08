28cb147bdf2d23c3749792be887c32f3c1e0621b
ausi1997.com <ausafen1018@gmail.com>
2021-03-08 23:16:54 +0530

    React setup with title page

---
+++ forogram/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = ()=>{
+    return(
+        <div className="title">
+        <h1>FotoGram</h1>
+        <h2>Your Photos</h2>
+        <p>You can post your photo here...</p>
+        </div>
+    )
+}
+
+export default Title;
